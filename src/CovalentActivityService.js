9fa0ca9c0032657d04a3fd2ebe7f88d35720899e
Alex Wait <alex.wait@emberex.com>
2012-11-13 01:26:21 +0000

    ILRN-55055: Refactor for safer saving and to prevent freezing when a request times out.

--- src/CovalentActivityService.js
+++ src/CovalentActivityService.js
@@ -7,9 +7,7 @@
      */
     function CovalentActivityService(covalentHost, useSecureProtocol, serviceBasePath, covalentHostTimeout)
     {
-
         if (! covalentHost) {
-        
             // The filename string "@cam.script.filename@" is dynamically replaced during the build process
             // to reflect the cam generated filename needed when resolving the Covalent Host. 
             covalentHost = CovalentUtils.resolveCovalentHost("@cam.script.filename@");
@@ -26,7 +24,6 @@
     };
     
     jQuery.extend(CovalentActivityService.prototype, {
-    
         _prepareLaunchParams: function(params)
         {
             // Serialize activityOptions if specified and an object
@@ -38,34 +35,34 @@
         launchSecureActivity: function(onSuccess, parameters, onFailure)
         {
             this._prepareLaunchParams(parameters);
-            this.restService.postRequestJSON("/secure", parameters, onSuccess, onFailure);    
+            return this.restService.postRequestJSON("/secure", parameters, onSuccess, onFailure);    
         },
         
         launchActivity: function(onSuccess, parameters, onFailure)
         {
             this._prepareLaunchParams(parameters);
-            this.restService.postRequestJSON("", parameters, onSuccess, onFailure);
+            return this.restService.postRequestJSON("", parameters, onSuccess, onFailure);
         },
         
         launchReviewActivity: function(onSuccess, parameters, onFailure)
         {
             this._prepareLaunchParams(parameters);
-            this.restService.postRequestJSON("/review", parameters, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/review", parameters, onSuccess, onFailure);
         },
         
         getActivityStatus: function(onSuccess, uuid, onFailure)
         {
-            this.restService.postRequestJSON("/status/" + uuid, null, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/status/" + uuid, null, onSuccess, onFailure);
         },
         
         getActivityState: function(onSuccess, uuid, onFailure)
         {
-            this.restService.getJSON("/state/"+uuid, null, onSuccess, onFailure);
+            return this.restService.getJSON("/state/"+uuid, null, onSuccess, onFailure);
         },
         
         submitActivityByUuid: function(onSuccess, uuid, onFailure)
         {
-            this.restService.postRequestJSON("/submit/"+uuid, null, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/submit/"+uuid, null, onSuccess, onFailure);
         },
         
         getItemWidgetParams: function(onSuccess, activityLocator, itemUid, options, mode, onFailure)
@@ -74,32 +71,31 @@
                 options: JSON.stringify(options),
                 mode: mode
             };
-            this.restService.getJSON("/"+activityLocator+"/item/"+itemUid, params, onSuccess, onFailure);
+            return this.restService.getJSON("/"+activityLocator+"/item/"+itemUid, params, onSuccess, onFailure);
         },
         
         gotoStep: function(onSuccess, activityLocator, stepUid, onFailure)
         {
-            this.restService.postRequestJSON("/"+activityLocator+"/step/"+stepUid+"/goto", null, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/"+activityLocator+"/step/"+stepUid+"/goto", null, onSuccess, onFailure);
         },
         
         loadActivity: function(onSuccess, activityLocator, onFailure)
         {
-            this.restService.getJSON("/"+activityLocator, null, onSuccess, onFailure);
+            return this.restService.getJSON("/"+activityLocator, null, onSuccess, onFailure);
         },
         
         nextStep: function(onSuccess, activityLocator, onFailure)
         {
-            this.restService.postRequestJSON("/"+activityLocator+"/step/next", null, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/"+activityLocator+"/step/next", null, onSuccess, onFailure);
         },
         
         submitItemResult: function(onSuccess, activityLocator, encryptedScoreAndState, itemUid, itemAttempted, onFailure)
         {
-            
            var params = {
                 encryptedScoreAndState: encryptedScoreAndState,
                 itemAttempted: itemAttempted
             };
-            this.restService.postRequestJSON("/"+activityLocator+"/item/"+itemUid+"/result", params, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/"+activityLocator+"/item/"+itemUid+"/result", params, onSuccess, onFailure);
         },
         
         updateActivityState: function(onSuccess, activityLocator, updatedInfo, onFailure)
@@ -107,22 +103,20 @@
             var params = {
                 updatedInfo: JSON.stringify(updatedInfo)
             };
-            this.restService.postRequestJSON("/"+activityLocator+"/state", params, onSuccess, onFailure);
+            return this.restService.postRequestJSON("/"+activityLocator+"/state", params, onSuccess, onFailure);
         },
         
         submitActivity: function(onSuccess, activityLocator, onFailure)
         {
-            this.restService.postRequestString("/"+activityLocator, null, onSuccess, onFailure);
+            return this.restService.postRequestString("/"+activityLocator, null, onSuccess, onFailure);
         },
         
         getProperties: function(onSuccess, parameters, onFailure)
         {
             this._prepareLaunchParams(parameters);
-            this.restService.postRequestJSON("/properties", parameters, onSuccess, onFailure);    
+            return this.restService.postRequestJSON("/properties", parameters, onSuccess, onFailure);    
         }
-        
     });
     
     window.CovalentActivityService = CovalentActivityService;
-    
 })();
